a29be2af9d6884d1fe5e5b2d9e6eeb1d44ad3ade
FaheemTech <faheemtech1@gmail.com>
2019-05-10 16:15:42 +0530

    get request is working for users

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const redis = require("redis");
 
+// Creating Redis client
+const client = redis.createClient();
+
+client.on("connect", () => {
+    console.log("redis connected");
+});
+
 // set PORT
 const PORT = 5000;
 
@@ -22,8 +29,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // set method override
 app.use(methodOverride("_method"));
 
+// search page
 app.get("/", (req, res, next) => {
     res.render("searchUsers");
+});
+
+// search processing
+app.post("/user/search", (req, res, next) => {
+    let id = req.body.id;
+
+    client.hgetall(id, (err, obj) => {
+        if (!obj) {
+            res.render("searchUsers", {
+                error: "user does not exists"
+            });
+        } else {
+            obj.id = id;
+            res.render("details", {
+                user: obj
+            });
+        }
+    });
 });
 
 app.listen(PORT, () => console.log("server running at port: " + PORT));
